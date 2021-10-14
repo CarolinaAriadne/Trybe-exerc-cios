3d3cabb55c48df640ac38615558c3e4160057565
Carolina Ariadne <c.ariadneadv@gmail.com>
2021-10-13 23:58:02 -0300

    Exercecicios

---
+++ Módulo1_Fundamentos_Do_Desenvolvimento_Web/Introdução_JavaScript_LógicaDeProgramação/JavaSript_Logica_De_Programacao_e_Algoritmos/scripts1.js
@@ -0,0 +1,16 @@
+let guardaAsterisco = "";
+
+let numero = 5;
+
+for(let index = 0; index < numero ; index +=1){
+
+    guardaAsterisco += "*";
+
+}    
+
+for (let index = 0; index < numero ; index += 1) {
+
+    console.log(guardaAsterisco);
+
+
+}
---
+++ Módulo1_Fundamentos_Do_Desenvolvimento_Web/Introdução_JavaScript_LógicaDeProgramação/JavaSript_Logica_De_Programacao_e_Algoritmos/scripts2.js
@@ -0,0 +1,14 @@
+let n = 5;
+
+let guardaAsterisco = "";
+
+
+for (let linha = 0; linha < n; linha++) {
+
+  guardaAsterisco += "*";
+
+  console.log(guardaAsterisco);
+
+}
+
+
