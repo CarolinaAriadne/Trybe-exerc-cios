93c7e9a12b84f311cb0a7215332c2d348ecbac1a
Carolina Ariadne <c.ariadneadv@gmail.com>
2021-11-12 00:10:25 -0300

    Exercicio

---
+++ Módulo1_Fundamentos_Do_Desenvolvimento_Web/Introdução- à- JavaScript- ES6- e- Testes-Unitários/JavaScriptES6-Objects/Parte-II/script.2.js
@@ -0,0 +1,34 @@
+
+const lesson1 = {
+  materia: 'Matemática',
+  numeroEstudantes: 20,
+  professor: 'Maria Clara',
+  turno: 'manhã',
+};
+
+const lesson2 = {
+  materia: 'História',
+  numeroEstudantes: 20,
+  professor: 'Carlos',
+};
+
+const lesson3 = {
+  materia: 'Matemática',
+  numeroEstudantes: 10,
+  professor: 'Maria Clara',
+  turno: 'noite',
+};
+
+
+// Crie uma função para listar as keys de um objeto. Essa função deve receber um objeto como parâmetro.
+
+const listKeys =  (Object2) => {
+
+   const retorno =  Object.keys(Object2);
+
+   return retorno; 
+  
+}
+
+console.log(listKeys(lesson3));
+
